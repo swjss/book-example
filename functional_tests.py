ce52b0497769efa32e9b5acb378cf55eba3f91b1
swj <1370037533@qq.com>
2016-06-23 15:59:38 +0800

    second commit

--- functional_tests.py
+++ functional_tests.py
@@ -2,6 +2,23 @@
 
 
 from selenium import webdriver
+import unittest,time
+
+class NewViewOrTest(unittest.TestCase):
+    def setUp(self):
+        self.bowser=webdriver.Firefox()
+    def tearDown(self):
+        self.bowser.quit()
+    def test_can_start_a_list_and_retive_after(self):
+        self.bowser.get("localhost:8000")
+        self.assertIn("To-Do",self.bowser.title)
+        self.fail("fail to test")
+if __name__ == '__main__':
+    unittest.main()
+
+'''
 browser=webdriver.Firefox()
 browser.get("http://localhost:8000")
-assert "Django" in browser.title
+assert "To-Do" in browser.title
+browser.quit()
+'''
