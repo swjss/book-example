d7e01d10a3e499ea4e70eb668d678a0616742281
swj <1370037533@qq.com>
2016-06-23 18:34:03 +0800

    Functional test checks we can input a item

--- functional_tests.py
+++ functional_tests.py
@@ -2,6 +2,7 @@
 
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest,time
 
 class NewViewOrTest(unittest.TestCase):
@@ -12,7 +13,17 @@ class NewViewOrTest(unittest.TestCase):
     def test_can_start_a_list_and_retive_after(self):
         self.bowser.get("localhost:8000")
         self.assertIn("To-Do",self.bowser.title)
-        self.fail("fail to test")
+        header_text=self.bowser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do',header_text)
+        inputbox=self.bowser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a to-do item')
+        inputbox.send_keys("Buy peacock feathers")
+        inputbox.send_keys(Keys.ENTER)
+        table = self.bowser.find_element_by_id('id_list_table')
+        rows=table.find_elements_by_tag_name('tr')
+        self.assertTrue(rows.text=='1: Buy peacock feathers' for i in rows)
+
+        self.fail("final to test")
 if __name__ == '__main__':
     unittest.main()
 
