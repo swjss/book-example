c1ff77f1f6ebed579cb5f6c558cfce0b6d7b533f
swj <1370037533@qq.com>
2016-06-23 16:47:03 +0800

    first unit test and url mapping,dummy view

--- list/tests.py
+++ list/tests.py
@@ -1,9 +1,25 @@
+# coding=utf-8
+"""
+   首要的目的就是测试home_page ，也就是 /
+"""
 from django.test import TestCase
+from django.core.urlresolvers import resolve 
+"""
+    这个resolve的作用就是解析url根据解析结果返回views里面的函数
+"""
+from  list.views import home_page
+# 这个home_page 就是/的处理函数
+
 
 # Create your tests here.
-
+'''
 class sometest(TestCase):
     def test_some_method(self):
         self.assertEqual(1+1,3)
-
+'''
+class sometest(TestCase):
+    def test_resolve_root_url_to_home_page_view(self):
+        found=resolve('/')
+        self.assertEqual(found.func,home_page)
+        
 
