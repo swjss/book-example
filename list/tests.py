aed85e4581248325a42e482af7ccab250d57f784
swj <1370037533@qq.com>
2016-06-23 17:09:03 +0800

    basic view now returns minimal HTML

--- list/tests.py
+++ list/tests.py
@@ -4,6 +4,7 @@
 """
 from django.test import TestCase
 from django.core.urlresolvers import resolve 
+from django.http import HttpRequest
 """
     这个resolve的作用就是解析url根据解析结果返回views里面的函数
 """
@@ -21,5 +22,13 @@ class sometest(TestCase):
     def test_resolve_root_url_to_home_page_view(self):
         found=resolve('/')
         self.assertEqual(found.func,home_page)
-        
+    def test_home_page_return_html(self):
+        request=HttpRequest()
+        """
+        用户在游览器中请求网页时，django看到的就是这么个对象
+        """
+        response=home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b"<title>To-Do lists</title>",response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
 
--- list/views.py
+++ list/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 '''
 home_page 是首页的调用函数
 '''
-def home_page():
-    pass
+def home_page(request):
+    return HttpResponse(b"<html><title>To-Do lists</title></html>")
